76853e21987171d38c4e869aba41e4a6feacf671
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-09-28 18:54:57 -0700

    Project Image Setup

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class ProjectCreator
 {
-  static createProject(title, description, size, hue, white)
+  static createProject(title, description, images, imageTitles, size, hue, white)
   {
     const container = document.getElementById(`${size}-projects`);
     const project = document.createElement("div");
@@ -8,21 +8,55 @@ class ProjectCreator
     project.classList.add(`project-${size}`);
     project.style.backgroundColor = `hwb(${hue} ${white} 60%)`;
 
+    let imagesStr = ""
+    for (let i = 0; i < images.length; i++)
+    {
+      imagesStr += `<img src="Project Images/Cosmula/${images[i]}" alt="${imageTitles[i]}" class="card-image"></img>`
+    }
+
     project.innerHTML = `
       <h2 class="card-header" style="background-color: hwb(${hue} ${white} 20%);">${title}</h2>
-      <div class="card-body"><p>${description}</p></div>
+      <div class="card-body">
+        <div class="project-images-${size} project-images">${imagesStr}</div>
+        <p class="project-text-${size}">${description}</p>
+      </div>
     `;
 
     container.appendChild(project);
   }
 }
 
-ProjectCreator.createProject("Cosmula", "This is a description of Cosmula.", "large", 206, 30);
-ProjectCreator.createProject("Fried Panic", "This is a description of Fried Panic.", "large", 13, 50);
-ProjectCreator.createProject("Mineral Mayhem", "This is a description of Mineral Mayhem.", "medium", 50, 30);
-ProjectCreator.createProject("Lincoln's Path", "This is a description of Lincoln's Path.", "medium", 30, 50);
-ProjectCreator.createProject("Dark Dash", "This is a description of Dark Dash.", "small", 283, 30);
-ProjectCreator.createProject("Portfolio?", "This is a description of portfolio. Check out more of my experience in my resume<link>?", "small", 240, 50);
+ProjectCreator.createProject(
+  "Cosmula",
+  `This is a description of Cosmula.<br>
+  Here is a new line.`,
+  ["Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png", "Cosmula Screenshot 2.png", "Cosmula Screenshot 3.png"],
+  ["Cosmula Gif", "Cosmula Screenshot 1", "Cosmula Screenshot 2", "Cosmula Screenshot 3"],
+  "large", 206, 30);
+ProjectCreator.createProject(
+  "Fried Panic",
+  `This is a description of Fried Panic.`,
+  ["Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png"],
+  ["GIF 1", "GIF 2", "GIF 3", "GIF 4"],
+  "large", 13, 50);
+ProjectCreator.createProject(
+  "Mineral Mayhem",
+  `This is a description of Mineral Mayhem.`,
+  ["Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png"],
+  ["GIF 1", "GIF 2"],
+  "medium", 50, 30);
+ProjectCreator.createProject(
+  "Lincoln's Path",
+  `This is a description of Lincoln's Path.`,
+  ["Cosmula Screenshot 1.png", "Cosmula Screenshot 1.png"],
+  ["GIF 1", "GIF 2"],
+  "medium", 30, 50);
+ProjectCreator.createProject(
+  "Dark Dash",
+  `This is a description of Dark Dash.`,
+  ["Cosmula Screenshot 1.png"],
+  ["GIF 1"],
+  "small", 283, 30);
 
 const buttons = document.querySelectorAll('.tabs button, .footer-right button, .section-body button');
 const contents = document.querySelectorAll('.tab-content');
